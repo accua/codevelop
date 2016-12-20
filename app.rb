8e431f1538196dc00705bff020d43b42c2311d0e
Matt Eichsteadt <matteichsteadt@gmail.com>
2016-12-20 10:05:42 -0800

    start sign-up

--- app.rb
+++ app.rb
@@ -5,3 +5,13 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get '/sign_up' do
+  erb :sign_up
+end
+
+post '/sign_up' do
+  user = params[:user]
+  email = params[:email]
+  password = params[:password]
+end
